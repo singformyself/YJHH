44ae1fcacb7a3fde173b7ee12242eab42e1937b0
ningzhigao123 <n17767209594@sina.com>
2025-03-14 18:01:30 +0800

    首次提交1

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+# 初始化 SQLAlchemy
+db = SQLAlchemy(app)
+
 from app import routes
+from app.models import User  # 导入模型
+
+# 将 db 作为 app 的属性
+app.db = db
